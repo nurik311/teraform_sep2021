d6b06fdbe1e7697fa1ae59873591d718d0ab4aeb
EC2 Default User <ec2-user@ip-172-31-33-122.us-east-2.compute.internal>
2022-07-25 23:11:14 +0000

    Updated

--- session10/backend.tf
+++ session10/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "terraform-session-september-remote-backend"
-        key = "session9/dev/terraform.tfstate"
+        key = "session10/dev/terraform.tfstate"
         region = "us-east-1"
         dynamodb_table = "terraform-lock"
     }
--- session10/main.tf
+++ session10/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-    source = "../../modules/ec2"
+    source = "github.com/nurik311/teraform_sep2021/modules/ec2"
     ami = "ami-0cff7528ff583bf9a"
     instance_type = "t2.micro"
     env = "dev"
@@ -7,7 +7,6 @@ module "ec2" {
 }
 
 module "s3" {
-    source = "../../modules/s3"
+    source = "../modules/s3"
     env = "dev"
 }
-
